0ca3ac0b65c1965e4075fff1df79581f0d6d57b8
trithvik07 <rithvikthonukunuri@gmail.com>
2023-07-19 22:40:16 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,10 @@ var app = express()
  
 app.use(cors()) 
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://ulearn-beta.vercel.app/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://ulearn-beta.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
   });
  
